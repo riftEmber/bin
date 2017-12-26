dd7c6399bc1d4474dea6ba559d3d7f09cd48fc3e
Anna Rift <ultimateartificer@gmail.com>
2017-12-26 16:27:51 -0700

    Add touchpad settings confiugration script

---
+++ touchpad-settings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# set up touchpad sensitivity
+xinput --set-prop "FocalTechPS/2 FocalTech Touchpad" "libinput Tapping Enabled" 1
+xinput --set-prop "FocalTechPS/2 FocalTech Touchpad" "libinput Accel Speed" 0.8
