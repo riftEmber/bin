63d1bd162f9a13ff602a519e0481dcb80685de22
Anna Rift <ultimateartificer@gmail.com>
2019-10-14 14:41:14 -0600

    Move screenshots and wallpapers

--- screenshot.sh
+++ screenshot.sh
@@ -2,7 +2,7 @@
 
 # take a screenshot
 
-SCREENSHOT_DIR=~/Dropbox/screenshots/
+SCREENSHOT_DIR=~/Dropbox/pictures/screenshots/
 mkdir -p $SCREENSHOT_DIR
 scrot -e "mv \$f $SCREENSHOT_DIR"
 notify-send "Screenshot saved to $SCREENSHOT_DIR"
