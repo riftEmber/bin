362fb63fcfb91060a8a1a622bdf12bf5db2f6243
Anna Rift <ultimateartificer@gmail.com>
2020-12-16 23:21:56 -0700

    Add second monitor on VGA-1

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+xrandr --output HDMI-1 --primary
+
 # hacky solution to using only external monitor for laptop
 sleep 0.5
 xrandr --output eDP-1 --off
+
+xrandr --output VGA-1 --right-of HDMI-1
+
 i3-msg 'workspace 1'
