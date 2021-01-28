c7dbccbf25e229e51b748e71b953f60c1553b3e0
Anna Rift <ultimateartificer@gmail.com>
2021-01-28 11:34:59 -0700

    Adjust monitor.sh for new setup

--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-xrandr --output HDMI-1 --primary
-
-# hacky solution to using only external monitor for laptop
-sleep 0.5
-xrandr --output eDP-1 --off
-
-xrandr --output VGA-1 --right-of HDMI-1
-
-i3-msg 'workspace 1'
+xrandr --auto
+xrandr --output HDMI-1-0 --left-of eDP-1
+xrandr --output HDMI-1-0 --primary
