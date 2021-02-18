9b367c9a218f6e6fa80714f3538c2f67e5bb5551
Anna Rift <ultimateartificer@gmail.com>
2021-02-17 19:01:06 -0700

    Change screenshot destination to syncthing

--- screenshot.sh
+++ screenshot.sh
@@ -2,7 +2,8 @@
 
 # take a screenshot
 
-SCREENSHOT_DIR=~/Dropbox/pictures/screenshots/
+SCREENSHOT_DIR=~/syncthing/pictures/screenshots/
+
 mkdir -p $SCREENSHOT_DIR
 scrot -e "mv \$f $SCREENSHOT_DIR"
 notify-send "Screenshot saved to $SCREENSHOT_DIR"
