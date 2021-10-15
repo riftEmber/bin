1476b6956f69c4f4492975a3596d06bf81a48607
Anna Rift <ultimateartificer@gmail.com>
2021-10-15 12:09:04 -0600

    Yet another monitor update

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 xrandr --auto
-xrandr --output HDMI-1-0 --left-of eDP-1
-xrandr --output HDMI-1-0 --primary
+xrandr --output HDMI-1-1 --left-of eDP-1
+xrandr --output HDMI-1-1 --primary
