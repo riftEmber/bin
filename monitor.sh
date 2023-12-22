0f472cb3ef104045b0b24f98ed3eeacd3442eeb7
Anna Rift <ultimateartificer@gmail.com>
2023-12-22 14:03:19 -0700

    Monitor updates

--- monitor.sh
+++ monitor.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# skip if no external monitors are connected
-if [ `xrandr --listmonitors | head -n 1 | cut -d' '  -f 2` -eq 1 ]; then
+# sleep 3
+#skip if no external monitors are connected
+# if [ `xrandr --listmonitors | head -n 1 | cut -d' '  -f 2` -eq 1 ]; then
+if [ `xrandr --query | grep " connected" | wc -l` -eq 1 ]; then
     exit 0
 fi
+
+xrandr --auto
 
 # disable internal monitor
 xrandr --output eDP-1 --off
 # temporarily leave internal monitor for debugging
-#xrandr --output eDP-1 --same-as DP-1-0
+# xrandr --output eDP-1 --same-as DP-1-0
 
 # set primary monitor
 xrandr --output DP-1-0 --primary
